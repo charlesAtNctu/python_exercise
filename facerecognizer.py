1098090bb86fac27394701ac6d300476e6748bf9
Ubuntu <ubuntu@ip-172-31-46-108.us-west-2.compute.internal>
2016-09-01 06:51:32 +0000

    first program

---
+++ facerecognizer.py
@@ -0,0 +1,10 @@
+import cv2, os
+import numpy as np
+
+print cv2.__version__
+
+recognizer = cv2.face.createLBPHFaceRecognizer()
+recognizer = cv2.face.createLBPHFaceRecognizer();
+recognizer = cv2.face.createEigenFaceRecognizer();
+recognizer = cv2.face.createFisherFaceRecognizer();
+
